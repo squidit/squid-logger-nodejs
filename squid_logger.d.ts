8f19d6215cf0bb33042c51429227f7068566a706
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2023-03-24 19:21:49 -0300

    🏷️ atualizada tipagem de métodos para explicitar parâmetros opcionais

--- squid_logger.d.ts
+++ squid_logger.d.ts
@@ -1,13 +1,25 @@
-declare module 'squid-logger'{
-  export function Configure(stdOutLogLevel: any, cloudLoggingLogLevel: any, sensitiveFieldsObj: any): any;
-  export function Trace(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Debug(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Info(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Warn(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Error(err: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Fatal(err: any, req: any, res: any, user: any, skipLog: any): void;
+declare module 'squid-logger' {
+
+  export function Configure(
+    projectId: string, 
+    googleCloudCredentials: string, 
+    environment: string, 
+    applicationName: string,
+     version: string, 
+     stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+     cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+     sensitiveFieldsObj: Record<string, unknown>, 
+     applicationRepository?: string, 
+     applicationRevisionId?: string
+     ): void;
+  export function Trace(dataToLog: any, req?: any, res?: any, user?: any, skipLog: boolean = false): void;
+  export function Debug(dataToLog: any, req?: any, res?: any, user?: any, skipLog: boolean = false): void;
+  export function Info(dataToLog: any,  req?: any, res?: any, user?: any, skipLog: boolean = false): void;
+  export function Warn(dataToLog: any,  req?: any, res?: any, user?: any, skipLog: boolean = false): void;
+  export function Error(err: any,       req?: any, res?: any, user?: any, skipLog: boolean = false): void;
+  export function Fatal(err: any,       req?: any, res?: any, user?: any, skipLog: boolean = false): void;
   /**
-   * @deprecated This funcction should not be used. Use the "Error" function instead.
+   * @deprecated This function should not be used. Use the "Error" function instead.
    */
   export function ReportError(err: any, req: any, res: any, user: any): void;
 }
