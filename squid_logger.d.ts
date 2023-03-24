f20d749cef019ff35a052249a26feb5e9d3d270f
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2023-03-24 19:37:27 -0300

    🚨 correção de lint

--- squid_logger.d.ts
+++ squid_logger.d.ts
@@ -5,13 +5,13 @@ declare module 'squid-logger' {
     googleCloudCredentials: string, 
     environment: string, 
     applicationName: string,
-     version: string, 
-     stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
-     cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
-     sensitiveFieldsObj: Record<string, unknown>, 
-     applicationRepository?: string, 
-     applicationRevisionId?: string
-     ): void;
+    version: string, 
+    stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+    cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+    sensitiveFieldsObj: Record<string, unknown>, 
+    applicationRepository?: string, 
+    applicationRevisionId?: string
+  ): void;
   export function Trace(dataToLog: any, req?: any, res?: any, user?: any, skipLog: boolean = false): void;
   export function Debug(dataToLog: any, req?: any, res?: any, user?: any, skipLog: boolean = false): void;
   export function Info(dataToLog: any,  req?: any, res?: any, user?: any, skipLog: boolean = false): void;
