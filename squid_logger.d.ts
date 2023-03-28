975d1648918f3127c4024d0aa5652321e3c714ce
Filipe Dircksen <33484438+dircksen@users.noreply.github.com>
2023-03-28 16:07:59 -0300

    fix: updated types on ts file for optional parameters (#5)

--- squid_logger.d.ts
+++ squid_logger.d.ts
@@ -1,13 +1,18 @@
 declare module 'squid-logger'{
-  export function Configure(stdOutLogLevel: any, cloudLoggingLogLevel: any, sensitiveFieldsObj: any): any;
-  export function Trace(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Debug(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Info(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Warn(dataToLog: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Error(err: any, req: any, res: any, user: any, skipLog: any): void;
-  export function Fatal(err: any, req: any, res: any, user: any, skipLog: any): void;
+  export function Configure(
+    stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+    cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null, 
+    sensitiveFieldsObj: Record<string, unknown>, 
+    ): void;
+
+  export function Trace(dataToLog: any, req: any, res: any, user: any, skipLog: boolean): void;
+  export function Debug(dataToLog: any, req: any, res: any, user: any, skipLog: boolean): void;
+  export function Info(dataToLog: any,  req: any, res: any, user: any, skipLog: boolean): void;
+  export function Warn(dataToLog: any,  req: any, res: any, user: any, skipLog: boolean): void;
+  export function Error(err: any,       req: any, res: any, user: any, skipLog: boolean): void;
+  export function Fatal(err: any,       req: any, res: any, user: any, skipLog: boolean): void;
   /**
-   * @deprecated This funcction should not be used. Use the "Error" function instead.
+   * @deprecated This function should not be used. Use the "Error" function instead.
    */
   export function ReportError(err: any, req: any, res: any, user: any): void;
 }
