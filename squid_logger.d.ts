72f99a7bd6355b16bf3e9f7ebfd80558d8d2f7c7
hmkmartini <hmk.martini@gmail.com>
2023-05-28 20:08:53 -0300

    feat: now exports .d.ts files as modules for squid-observability-nodejs support feat: updated squid-error-nodejs and squid-observability-configs-nodejs which now exports .d.ts files as modules for squid-observability-nodejs support

--- squid_logger.d.ts
+++ squid_logger.d.ts
@@ -1,16 +1,15 @@
 declare module 'squid-logger'{
   export function Configure(
-    stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null,
-    cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null,
+    stdOutLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null | undefined,
+    cloudLoggingLogLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | null | undefined,
     sensitiveFieldsObj: Record<string, unknown>,
     ): void;
-
-  export function Trace(dataToLog: any, req: any, res: any, user: any, skipLog: boolean, labels: any): void;
-  export function Debug(dataToLog: any, req: any, res: any, user: any, skipLog: boolean, labels: any): void;
-  export function Info(dataToLog: any,  req: any, res: any, user: any, skipLog: boolean, labels: any): void;
-  export function Warn(dataToLog: any,  req: any, res: any, user: any, skipLog: boolean, labels: any): void;
-  export function Error(err: any,       req: any, res: any, user: any, skipLog: boolean, labels: any): void;
-  export function Fatal(err: any,       req: any, res: any, user: any, skipLog: boolean, labels: any): void;
+  export function Trace(dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
+  export function Debug(dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
+  export function Info (dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
+  export function Warn (dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
+  export function Error(dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
+  export function Fatal(dataToLog: any, req?: any, res?: any, user?: string, skipLog?: boolean, labels?: { sink: 'LGPD' } | Record<string, string>): void;
   /**
    * @deprecated This function should not be used. Use the "Error" function instead.
    */
